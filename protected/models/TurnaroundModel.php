3d7e58a056c4bfc6c5605f76b538b5775a70559b
Alex <Alex@alex-laptop>
2013-06-02 00:11:43 +0400

    Correct Flight Model. Insert Turnaround Model.

--- protected/models/TurnaroundModel.php
+++ protected/models/TurnaroundModel.php
@@ -1,24 +1,25 @@
 <?php
 /**
- * @file FlightModel.php
+ * @file TurnaroundModel.php
  * 
  * @autor Alex Letov
  * 
- * Flight model.
+ * Turnaround model.
  */
 
-class Flight extends CActiveRecord {
+class Turnaround extends CActiveRecord {
     public function getDbConnection(){
         return Yii::app()->db;
     }
  
     public function tableName(){
-         return 'flight';
+         return 'turnaround';
     }
     
     public function relations() {
         return array(
-            'airport' => array(self::BELONGS_TO, 'Airport', 'id'),
+            'to' => array(self::HAS_ONE, 'Flight', 'flttoid'),
+            'from' => array(self::HAS_ONE, 'Flight', 'fltfromid'),
         );
     }
 }
